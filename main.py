86497fd4077b6436ee582210e097bfe4bc72e467
Matias Damian Duque de Arce <duque_damian@hotmail.com>
2023-03-19 14:01:30 -0300

    API con pandas

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pymysql.cursors
 from fastapi import FastAPI
 import pandas as pd
 
@@ -7,25 +6,15 @@ app = FastAPI(title= "API para consultar datos, en plataformas de Streaming",
     description= "Amazon, Disney, Hulu, Netflix")
 
 
-# Conectar a la base de datos
-connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    db='pi_ml_ops',
-    cursorclass=pymysql.cursors.DictCursor
-)
-
 @app.on_event('startup')
 def startup():
-    global plataformas
+    global df; global df_score; global plataformas
+    df = pd.read_csv('Datasets/plataformas.csv', delimiter=';')
+    df_score = pd.read_csv('Datasets/score_users.csv')
     plataformas = ['amazon', 'hulu', 'netflix', 'disney']
 
-@app.get("/menu")
-def menu():
-    return ("Funciones de mi API: (1) get_max_duration() (2) get_score_count() (3) get_count_platform (4) get_actor")
 
-# Título de más duración, por plataforma y por año:  
+# Título de más duración, por plataforma, año y tipo de duración:  
 # URL para realizar la consulta /get_max_duration(año,'plataforma','[min o season]')
 @app.get('/get_max_duration({year},{platform},{duration_type})')
 def get_max_duration(year:int, platform:str, duration_type:str):
@@ -35,53 +24,43 @@ def get_max_duration(year:int, platform:str, duration_type:str):
     duration_type = duration_type.replace("'","")
     duration_type = duration_type.lower()
     if duration_type not in ('min','season')  : return 'Se debe especificar el tipo de duración como "min" o "season"'   # Verificación de tipo
-    with connection.cursor() as cursor:
-        cursor.execute('''SELECT * FROM plataforma where release_year=(%s) and plataform=(%s) and duration_type=(%s) 
-                        order by duration_int desc limit 1''',(year,platform,duration_type))
-        Max_duration = cursor.fetchall()
-        Max_duration = Max_duration[0]
-    return f'El título de la plataforma {platform}, del año {year} con mayor duración es: {Max_duration["title"].title()}, con una duración de: {Max_duration["duration_int"]} {duration_type}'
- #agregas los otros datos
+    max_duration = df[(df.plataform == platform) & (df.release_year == year) & (df.duration_type == duration_type)]
+    if max_duration.shape[0] == 0: return f'Sin datos del año {year}'  # Verificacion de año
+    # Retornamos el valor del título
+    id_max = max_duration.duration_int.idxmax()
+    return f'El título de la plataforma {platform}, del año {year} con mayor duración es: {max_duration.loc[id_max].title.title()}, con una duración de: {max_duration.loc[id_max].duration_int} {duration_type}.'
 
 # Cantidad de películas por plataforma con un puntaje mayor a XX en determinado año. El scored debe ser una numero comprendido entre 1 a 100
-@app.get("/get_score_count({platform},{scored},{year})")
+@app.get('/get_score_count({platform},{scored},{year})')
 async def get_score_count(platform: str, scored: float, year:int):
     platform = platform.replace("'","")
     platform = platform.lower()
     if platform not in plataformas: return f'Sin datos para la plataforma: {platform}'
     if not 1 <= scored <= 100 :  return f'El scored no esta dentro del rango 1 a 100'
-    with connection.cursor() as cursor:
-        cursor.execute('''select count(id) cantidad from plataforma p join score_usuarios s on (s.movie_id=p.id) 
-                        where p.plataform = (%s) and p.release_year = (%s) and s.score > (%s)''',(platform,year,scored))
-        Total_movies = cursor.fetchall()
-    if Total_movies[0]["cantidad"] == 0: return f'Sin datos para el año {year}'
-    return f'La cantidad de peliculas de la plataforma {platform}, del año {year}, con un scored mayor a {scored} es de: {Total_movies[0]["cantidad"]}'
+    df_add_score = pd.merge(df, df_score, how='inner', on = 'id')
+    score_count = df_add_score[(df_add_score.plataform == platform) & (df_add_score.release_year == year) & ((df_add_score.score > scored) )]
+    if score_count.shape[0] == 0: return f'Sin datos del año {year}'
+    return f'La cantidad de peliculas de la plataforma {platform}, del año {year}, con un scored mayor a {scored} es de: {score_count.id.count()}.'
 
-# Total de películas, por plataforma:
+# Total de películas por plataforma:
 # URL para realizar la consulta /get_count_platform('platform')
 @app.get('/get_count_platform({platform})')
 async def get_count_platform(platform:str):
     platform = platform.replace("'","")
     platform = platform.lower()
     if platform not in plataformas: return f'Sin datos para la plataforma: {platform}'
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT count(*) cantidad FROM plataforma where plataform = (%s)',(platform) )
-        Total_movies = cursor.fetchall()
-    return f'La cantidad total de peliculas de la plataforma {platform} es de: {Total_movies[0]["cantidad"]}'
+    count = df[(df.plataform == platform)].id.count()
+    return f'La cantidad total de títulos de la plataforma {platform} es de: {count}.'
 
 
 
-# Actor con mayor ocurrencias, por plataforma y por año:
+# Actor con mayor ocurrencias, por plataforma y año:
 # URL para realizar la consulta /get_actor('Netflix',2018)
 @app.get('/get_actor({platform},{year})')
 async def get_actor(platform:str,year:int):
     platform = platform.replace("'","")
     platform = platform.lower()
     if platform not in plataformas: return f'Sin datos para {platform}'
-    with connection.cursor() as cursor:
-        cursor.execute("select * from plataforma")
-        Total_movies = cursor.fetchall() 
-    df = pd.DataFrame(Total_movies)
     actores, repeticiones = list(), list()  
     cast_list = list(df[(df.plataform == platform) & (df.release_year == year)].cast.fillna('Sin_dato'))
     for each in cast_list:  
@@ -95,4 +74,4 @@ async def get_actor(platform:str,year:int):
                     actores.append(elem)
                     repeticiones.append(1)
     if actores == []: return f'Sin datos para el año {year}' 
-    return f'El actor que más se repite en la plataforma {platform}, en el año {year}, es {actores[repeticiones.index(max(repeticiones))].title()}, con un total de {max(repeticiones)} repeticiones'
+    return f'El actor que más se repite en la plataforma {platform}, en el año {year}, es {actores[repeticiones.index(max(repeticiones))].title()}, con un total de {max(repeticiones)} repeticiones.'
